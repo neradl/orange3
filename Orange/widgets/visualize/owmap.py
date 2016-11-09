4d235fa86ff7f1b33e0f82b265c59a83317159be
Kernc <kerncece@gmail.com>
2016-11-09 16:50:29 +0100

    OWMap: show progress bar indicator while updating markers

--- Orange/widgets/visualize/owmap.py
+++ Orange/widgets/visualize/owmap.py
@@ -374,6 +374,8 @@ class LeafletMap(WebviewWidget):
 
         self._image_token = image_token = np.random.random()
 
+        n_iters = np.ceil(len(visible) / N_POINTS_PER_ITER)
+
         def add_points():
             nonlocal cur, image_token
             if image_token != self._image_token:
@@ -418,7 +420,12 @@ class LeafletMap(WebviewWidget):
             cur += N_POINTS_PER_ITER
             if cur < len(visible):
                 QTimer.singleShot(10, add_points)
+                self._owwidget.progressBarAdvance(100 / n_iters, None)
+            else:
+                self._owwidget.progressBarFinished(None)
 
+        self._owwidget.progressBarFinished(None)
+        self._owwidget.progressBarInit(None)
         QTimer.singleShot(10, add_points)
 
 
